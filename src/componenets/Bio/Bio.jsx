0470a0033c768320db7d70319d0b29acb516d221
Ollie Robins <ollierobins93@gmail.com>
2020-10-16 11:27:50 +0100

    added bio folders

---
+++ src/componenets/Bio/Bio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Bio.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Bio = () => {
+    return (
+        <p>this is a test</p>
+    );
+}
+
+export default Bio;
