8ea1d1f727b1322f46eb0e3eedea5306fd4d233e
Ollie Robins <ollierobins93@gmail.com>
2020-10-30 09:47:14 +0000

    amended text

--- src/data/text.js
+++ src/data/text.js
@@ -1,7 +1,7 @@
 const Text = [
     {
-    'bio1' : `Having always had a keen interest in technology, paired with a passion for problem solving, I joined the fascinating world of software development at the start of 2020. I enrolled on the Nology 12 week software development course in September to enhance my skills further.`,
-    'bio2' : `The tech stack I currently work with is below. I also gained experience working withing an Agile team, pair programming, mentoring other students who have just started the course as well as my background in Sales roles.`
+    'bio1' : `Having always had a keen interest in technology, paired with a passion for problem solving, I joined the fascinating world of software development at the end of 2019. I enrolled on the Nology 12 week software development course in September to enhance my skills further.`,
+    'bio2' : `The tech stack I currently work with is mern. I also gained experience working within an Agile team, pair programming, developing and delivering a client project and mentoring other students who have just started the course. `
     }
 ]
 
