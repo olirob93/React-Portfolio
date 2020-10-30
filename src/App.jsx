5ec4c6a36902b680aac3b90704611719a14c5e64
Ollie Robins <ollierobins93@gmail.com>
2020-10-30 08:44:16 +0000

    added back in font awesome

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.module.scss';
 import Navs from './containers/Navs';
 import Routes from './containers/Routes';
+import library from './data/fa-library';
 
 const App = () => {
 
