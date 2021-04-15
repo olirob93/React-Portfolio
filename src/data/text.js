33c8597cc87b8faa28194fea13939aecc471dd02
Ollie Robins <ollierobins93@gmail.com>
2021-04-15 12:30:37 +0100

    spelling changes

--- src/data/text.js
+++ src/data/text.js
@@ -1,7 +1,7 @@
 const Text = [
     {
     'bio1' : `Having always had a keen interest in technology, paired with a passion for problem solving, I joined the fascinating world of software development at the end of 2019. I enrolled on the Nology 12 week software development course in September to enhance my skills further.`,
-    'bio2' : `Since finsihing the course I secured a role working in a small team building and maintaining a tech stack that uses React, Redux, Node and deploys via AWS. I am currently working to rebuild part of this website that uses older web development technologies. I am using Gatsby for this and integrating Strapi CMS for markting and other non-technical staff to manage blog posts and other conentent for the website on the fly. `,
+    'bio2' : `Since finsihing the course I secured a role working in a small team building and maintaining a tech stack that uses React, Redux, Node, and deploys via AWS. I am currently working to rebuild part of this website that uses older web development technologies. I am using Gatsby for this and integrating Strapi CMS for markting and other non-technical staff to manage blog posts and other content for the website on the fly. `,
     'bio3' : `When i'm not working on my coding skills, I like to enjoy my downtime by doing a number of different things. I'm a keen golfer with an active handicap of 4, I've recently got into running and love travelling to new places and seeing different cultures. I recently visited Sri-Lanka which I would highly recommend. `
     }
 ]
